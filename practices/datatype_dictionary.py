7fe596e29cddec4156ab1f410b4ced8948f71f45
Peter Lin <peterjunlin@gmail.com>
2021-02-02 19:30:52 -0800

    language_variable_scope

--- practices/datatype_dictionary.py
+++ practices/datatype_dictionary.py
@@ -1,13 +1,24 @@
-def practice1():
-    dict_definition()
-
-
-def dict_definition():
+def dict_initialization():
     # Set empty dictionary
-    dict1 = {}
+    dict1 = dict()
+
+    # Use constructor
+    d = dict(a=1, b=2, c=3, d=4, e=5)
+
+    # Use literal
+    d = {'a':1, 'b':2}
+
+
+def dict_properties():
+    dict1 = dict(one=1)
+    d = dict(a=1, b=2, c=3, d=4, e=5)
 
     # Get type of dictionary
     print("type(dict1): ", type(dict1))
+
+
+def dict_operations():
+    dict1 = dict{}
 
     # Add item and set value
     dict1["a"] = 1
@@ -23,4 +34,7 @@ def dict_definition():
     print('dict1["a"] = dict1.get("a", 0) + 1   ', dict1)
 
 
-practice1()
+if __name__ == "__main__":
+    dict_initialization()
+    dict_properties()
+    dict_operations()
--- practices/language_class.py
+++ practices/language_class.py
@@ -1,14 +1,11 @@
-print("This line is at the beginning of the module, this will run at time when this module is imported.")
-print("Once the module was imported, the later importation will be skipped.")
-
 class Shape:
     """
     Find the executing sequence of each section of module and class.
     Find how to use class member variable in and out of class method and instance method.
     """
     member1 = 10
-    print("This line is at the beginning of class definition, which will run.")
-    print("Only run once and this won't run in derived classes")
+    print("This line is at the beginning of class definition, it will run when importing.")
+    print("Only run once and this won't run in derived classes.")
 
     def __init__(self):
         print("Constructor")
@@ -20,24 +17,18 @@ class Shape:
     def method2(self):
         print('In instance method, self.member1', self.member1)  # access instance-member variable
 
-    print("This line is at the end of class definition.")
+    print("This line is at the end of class definition. "
+          "This line will run too, without need to create an instance.")
 
-def practice1():
-    print("After this line, create an instance of Shape class.")
-    e = Shape()
-    # print(type(e))
-    # print(isinstance(e, Shape))
-    Shape.method1()  # access class-member
-    e.member1 = 20  # access instance-member variable
-    e.method1()
-    e.method2()
 
-class A:
+class Polygon(Shape):
+    print("Polygon class loading.")
     _abc_ = 0
     __def = 0
 
     def __init__(self):
         self.x = 0
+        print(dir())
 
     def func1(self):
         self.x = 1
@@ -50,11 +41,16 @@ class A:
     def func3(self):
         print(dir(self))  # this can display the mingled double underscore name _A__def
 
-class B(Shape):
-    print("Is this line run if create no instance -- yes!")
 
-def practice2():
-    k = A()
-    k._abc_ = 1
-    # k.__def = 1
-    k.func3()
+if __name__ == "__main__":
+    print("After this line, create an instance of Shape class.")
+    e = Shape()
+    p = Polygon()
+
+    # print(type(e))
+    # print(isinstance(e, Shape))
+    Shape.method1()  # access class-member
+    e.member1 = 20  # access instance-member variable
+    e.method1()
+    e.method2()
+
