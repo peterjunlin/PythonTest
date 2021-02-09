ec25de183e634b2aa4ad51198502be3c06396c12
Peter Lin <peterjunlin@gmail.com>
2021-02-08 18:52:49 -0800

    datatype_dictionary

--- practices/datatype_dictionary.py
+++ practices/datatype_dictionary.py
@@ -2,11 +2,14 @@ def dict_initialization():
     # Set empty dictionary
     dict1 = dict()
 
-    # Use constructor
+    # With list of key-value pair
+    dict1 = dict([('a', 1), ('b', 2), ('c', 3)])
+
+    # Use keyword arguments
     d = dict(a=1, b=2, c=3, d=4, e=5)
 
     # Use literal
-    d = {'a':1, 'b':2}
+    d = {'a': 1, 'b': 2}
 
 
 def dict_properties():
@@ -37,8 +40,45 @@ def dict_operations():
     dict3 = {**dict2, 'k': 4}  # unpacking dictionary
     assert dict3 == {'x': 1, 'y': 2, 'z': 3, 'k': 4}
 
+    # find
+    assert 'x' in dict2
+
+    assert list(dict2.keys()) == ['x', 'y', 'z']
+    assert list(dict2.values()) == [1, 2, 3]
+    assert list(dict2.items()) == [('x', 1), ('y', 2), ('z', 3)]
+
+    # Combine 2 lists as dictionary
+    list1 = ['a', 'b', 'c']
+    list2 = [1, 2, 3]
+    dict1 = dict(zip(list1, list2))
+    assert dict1 == {'a': 1, 'b': 2, 'c': 3}
+
+    # order is not significant in dictionary
+    assert {'a': 1, 'b': 2, 'c': 3} == {'a': 1, 'c': 3, 'b': 2}
+
+    del dict1['a']
+    assert dict1 == {'b': 2, 'c': 3}
+
+
+def dict_comprehension():
+    dict1 = {x: x**2 for x in [1, 2, 3]}
+    assert dict1 == {1: 1, 2: 4, 3: 9}
+
+    dict2 = {'a': 1, 'b': 2, 'c': 3}
+    dict3 = {k: v*2 for (k, v) in dict2.items() if v < 3}
+    assert dict3 == {'a': 2, 'b': 4}
+
+
+def dict_looping():
+    dict1 = {'a': 1, 'b': 2, 'c': 3}
+    for k, v in dict1.items():
+        dict1[k] = v * 2
+    assert dict1 == {'a': 2, 'b': 4, 'c': 6}
+
 
 if __name__ == "__main__":
     dict_initialization()
     dict_properties()
     dict_operations()
+    dict_comprehension()
+    dict_looping()
