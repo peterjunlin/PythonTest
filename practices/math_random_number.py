35705700f57d1f28c301710a347c8dacbc078d28
Peter Lin <peterjunlin@gmail.com>
2021-02-13 11:58:28 -0800

    math_random_number

--- practices/math_random_number.py
+++ practices/math_random_number.py
@@ -1,5 +1,6 @@
 """
-random.randint(1, 6)
+random.randint(0, 1) -- sequence of 0, 1
+random.randint(0, 2) -- sequence of 0, 1, 2
 """
 
 import random
@@ -7,8 +8,8 @@ import random
 
 def random_when_repeat():
     """how long the random sequence can repeat a number in a row."""
-    num_cases = 100
-    num_repeated = 6
+    num_cases = 1000
+    num_repeated = 5
 
     average_i_repeated = 0
     for i in range(num_cases):
@@ -17,7 +18,7 @@ def random_when_repeat():
         i_when_repeated = 0
         while count_repeated < num_repeated:
             i_when_repeated += 1
-            t = random.randint(0, 10)
+            t = random.randint(0, 1)  # (0, 1) sequence
             # print(i_when_repeated, t)
             if t == t0:
                 count_repeated += 1
@@ -28,7 +29,7 @@ def random_when_repeat():
         print(i_when_repeated)
         average_i_repeated += i_when_repeated
 
-    print("Average = ", average_i_repeated / num_cases)
+    print("Average = ", average_i_repeated / num_cases)  # close to 2 ** (num_repeated + 1)
 
 
 if __name__ == '__main__':
